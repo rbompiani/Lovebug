ba842730f50e9f6a44aeaecbf01e3568adfd669b
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-05-15 12:34:42 -0700

    added basic mobile skeleton

---
+++ lovebug-mobile/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Set Servo Angle:</Text>
+      <TextInput
+        autoCompleteType="off"
+        autoCorrect={false}
+        autoFocus={true}
+        style={styles.input}
+        placeholder="0 - 180"
+        onChange={() => { }}
+      />
+      <View style={styles.buttonContainer}>
+        <View style={styles.button} >
+          <Button title="Send" />
+        </View>
+        <View style={styles.button} >
+          <Button title="Clear" />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    marginVertical: 20,
+    fontSize: 30
+  },
+  input: {
+    width: '80%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    fontSize: 80,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  buttonContainer: {
+    width: '100%',
+    flexDirection: "row",
+    justifyContent: "center",
+    marginVertical: 20
+  },
+  button: {
+    width: 100,
+    margin: 10
+  }
+});
